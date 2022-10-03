9f140ea48c4cb7c5dd4c1e75afa5e36fd47f64d7
Weidows <utsuko27@qq.com>
2022-10-03 21:38:32 +0800

    backup.

--- others/PowerShell/Microsoft.PowerShell_profile.ps1
+++ others/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,36 @@
 # ======================================================================== #
-#                               pwsh配置文件
+#                               template
 # ======================================================================== #
+# Alias
+# Set-Alias -Name code        -Value code-insiders
+# Set-Alias -Name open        -Value explorer
+# Set-Alias -Name c           -Value CODEHERE
+# Set-Alias -Name o           -Value OPENHERE
 
-# PoShFuck
-Import-Module PoShFuck
+# Define
+# Function OPENHERE    { Start-Process -FilePath "explorer" -ArgumentList "."}
+# Function CODEHERE    { Start-Process -FilePath "code-insiders" -ArgumentList "."}
 
-# git-tab补全
-Import-Module posh-git
 
-# terminal-icons
-Import-Module terminal-icons
+# ======================================================================== #
+#                               pwsh配置
+# ======================================================================== #
+# 设置主题 (Get-PoshThemes $env:SCOOP\persist\oh-my-posh\themes) / https://ohmyposh.dev/docs/themes
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/iterm2.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/night-owl.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/powerlevel10k_classic.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/poshmon.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/slimfat.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/takuya.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/thecyberden.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:SCOOP/persist/oh-my-posh/themes/wholespace.omp.json" | Invoke-Expression
 
-# 设置主题
-# Set-PoshPrompt -Theme powerlevel10k_rainbow
-# Set-PoshPrompt -Theme blueish
-Set-PoshPrompt -Theme atomic
+# PoShFuck: https://github.com/mattparkes/PoShFuck
+# Import-Module PoShFuck
 
-# scoop-completion
-Import-Module $env:SCOOP\apps\scoop\current\supporting\completion\Scoop-Completion.psd1 -ErrorAction SilentlyContinue
+# 设置终端代理 (终端默认不会走代理)
+$Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
 
 # Enable Prediction History
 Set-PSReadLineOption -PredictionSource History
@@ -27,8 +40,53 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-# 设置终端代理 (终端默认不会走代理)
-$Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
+# https://github.com/yuk7/dotfiles/blob/main/_pwsh/profile.ps1
+function x{exit}
+function c{cls}
+function e{echo $args}
+function ..{cd ..}
+function ...{cd ../..}
+function ....{cd ../../..}
+function .....{cd ../../../..}
+function ~{cd $env:USERPROFILE}
+function /{cd \}
+function \{cd \}
+
+
+# ======================================================================== #
+#                               scoop
+# ======================================================================== #
+# scoop-completion
+Import-Module $env:SCOOP\apps\scoop\current\supporting\completion\Scoop-Completion.psd1 -ErrorAction SilentlyContinue
+Invoke-Expression (&scoop-search-multisource --hook)
+
+function sct{scoop status}
+function scl{scoop list}
+function scu{
+  Foreach($argv in $args) {
+    scoop update $argv
+    scoop cleanup $argv
+    echo ========================================================================
+  }
+}
+function scun{scoop uninstall $args}
+function scs{scoop search $args}
+function sci{
+  Foreach($argv in $args) {
+    scoop install $argv
+    echo ========================================================================
+  }
+}
+function scin{scoop info $args}
+
+# terminal-icons
+# Import-Module terminal-icons
+Import-Module $env:SCOOP\modules\Terminal-Icons
+# git-tab补全
+# Import-Module posh-git
+# Import-Module $env:SCOOP\modules\posh-git
+# Import-Module $env:SCOOP\modules\posh-docker
+
 
 # 清屏
 # clear
